3cc540b8bbab814521bae5e3890b9b9af03c9c24
KavitaPatil1 <kavita.patil61296@gmail.com>
2024-03-28 14:43:48 +0530

    HomePage1

---
+++ src/main/java/com/withoutpagefactory/HomePage1.java
@@ -0,0 +1,12 @@
+package com.withoutpagefactory;
+
+import org.openqa.selenium.By;
+
+public class HomePage1 {
+	
+	
+	public By sneakersname=By.xpath("//input[@placeholder=\"Search for products, brands and more\"]");
+
+	
+
+}
